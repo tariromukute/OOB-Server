3e976e0039530d3a8d08b72795213d08adf31ea6
Tariro Mukute <tariromukute@gmail.com>
2022-03-17 08:21:13 +0200

    Changed log level and fixed start error * Update for loop for configuring domain names to avoid duplicates

--- configure.sh
+++ configure.sh
@@ -3,9 +3,9 @@
 # Setup the bind9
 ./setup.sh
 
-for j in {1..200}
+for j in {10..99}
 do
-  for i in {1..200}
+  for i in {10..99}
     do
     ./configure_domain.sh open${j}${i}lan.mk 172.19.${j}.${i}
     done
